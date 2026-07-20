cc40773ba336481fa3ef75df572e61aef78fe769
Chhorvin Kann <info.chhorvinkann9@gmail.com>
2026-07-20 21:03:43 +0700

    renew

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,35 @@
-<script setup>
-import { ref } from 'vue';
-
-
-const name = ref('Jame Deo');
-const status = ref('active');
-const tasks = ref([]);
-const newTask = ref('')
-
-const toggleStatus = () => {
-    if (status.value === 'active') {
-        status.value = 'pending'
-    } else if (status.value === 'pending') {
-        status.value = 'inactive';
-    } else {
-        status.value = 'active';
-    }
-};
-
-const addTask = () => {
-    if (newTask.value.trim() !== '') {
-        tasks.value.push(newTask.value);
-        newTask.value = '';
-    }
-}
-
-const deleteTask = (index) => {
-    tasks.value.splice(index, 1)
-};
-</script>
-
-
 <template>
-    <h1> {{ name }}</h1>
-    <p v-if="status === 'active'">user is active</p>
-    <p v-else-if="status === 'pending'">user is pending</p>
-    <p v-else>user is inactive</p>
 
-    <!-- form for tasks -->
-    <form @submit.prevent="addTask">
-        <label for="newTask">Add Task</label>
-        <input type="text" name="" id="newTask" v-model="newTask">
-        <button class="btnAdd" type="submit">Add new</button>
-    </form>
+    <!--Call from child to show on parent components -->
 
-    <h3 class="text-head">Add your plan here </h3>
-    <li v-for="(task, index) in tasks" :key="task">
-        <span>
-            <button class="btn-del" @click="deleteTask(index)">Delete</button>
-            {{ task }}
-        </span>
+    <Cart :title="dynamicTitle" :follower="followered" :isStatus="true" />
 
-    </li>
-    <button class="chStatus" @click="toggleStatus">Change</button>
 </template>
 
-<style>
-.text-head {
-    color: yellow;
-}
+<script setup>
+import { watch, reactive, ref } from 'vue'
+import Cart from '@/components/ProfileCard.vue';
 
-.btn-del {
-    width: 80px;
-    height: 20px;
-    border: none;
-    border-radius: 5px;
-    background-color: red;
-}
+// const users = reactive({
+//     name: "Joe",
+//     age: 20
+// });
+
+// const nameMessage = ref('')
+// const ageMessage = ref('')
+
+// watch(() => users.age, (newAge, oldAge) => {
+//     ageMessage.value = `age old changed from ${oldAge} to ${newAge}`
+//     console.log(ageMessage.value)
+// })
+
+// watch(() => users.name, (newUser, oldUser) => {
+//     nameMessage.value = `user old changed from ${oldUser} to ${newUser}`
+//     console.log(nameMessage.value)
+// })
+
+const dynamicTitle = ref('User Profile')
+const followered = ref(110)
 
 
-.btnAdd {
-    width: 80px;
-    height: 20px;
-    border: none;
-    border-radius: 5px;
-    background-color: greenyellow;
-}
-
-.chStatus {
-    width: 80px;
-    height: 20px;
-    border: none;
-    border-radius: 5px;
-    background-color: skyblue;
-}
-
-li {
-    list-style: none;
-}
-</style>
+</script>
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+// This child components
+
+const props = defineProps({
+    title: String,
+    follower: Number,
+    isStatus: Boolean
+})
+
+console.log(props.title)
+</script>
+
+<template>
+    <h1 class="cl">{{ title }}</h1> <br>
+    <p yl>Follower: {{ follower }}</p> <br>
+    <p gre>Status: {{ isStatus ? 'Online' : 'Offline' }}</p>
+</template>
+
+<style>
+.cl {
+    color: aqua;
+}
+</style>
